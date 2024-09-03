2f6a8c689e885be5cb20f246fb588837382a9ebf
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-09-03 05:05:37 +0300

    fiber project architecture init

---
+++ configuration/config.go
@@ -0,0 +1,25 @@
+package configuration
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config interface {
+	Get(key string) string
+}
+
+type configImpl struct {
+}
+
+func (config *configImpl) Get(key string) string {
+	return os.Getenv(key)
+}
+
+func New(filenames ...string) Config {
+	err := godotenv.Load(filenames...)
+	log.Fatal(err)
+	return &configImpl{}
+}
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"gofiber-clean-architecture/configuration"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config := configuration.New()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World! Go Fiber")
 	})
 
-	app.Listen(":5000")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "6060" // Default port
+	}
+
+	log.Fatal(app.Listen(":" + port))
 }
